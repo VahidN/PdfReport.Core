be2b7f79d42a10cc45530003cc0b5dc2b94db449
VahidN <vahid_nasiri@yahoo.com>
2017-05-12 23:20:16 +0430

    Added a SampleWebApp

---
+++ PdfRpt.Core.SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using PdfRpt.Core.SampleWebApp.Reports;
+
+namespace PdfRpt.Core.SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            var reportBytes = InMemoryPdfReport.CreateInMemoryPdfReport(_hostingEnvironment.WebRootPath);
+            return File(reportBytes, "application/pdf", "report.pdf");
+        }
+    }
+}
