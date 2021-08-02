84fa23f7f08bcc68a6c4f9c3dacc60fbb5631b1e
Vahid Nasiri <vahid_nasiri@yahoo.com>
2021-08-02 14:06:58 +0430

    Add FUNDING.yml

--- update-dependencies.bat
+++ update-dependencies.bat
@@ -1,6 +1,4 @@
 dotnet restore
-setx DOTNET_HOST_PATH "%ProgramFiles%\dotnet\dotnet.exe" /M
-dotnet tool uninstall --global dotnet-outdated
 dotnet tool update --global dotnet-outdated-tool
 dotnet outdated -u
 dotnet restore
